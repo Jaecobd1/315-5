9f077d6b86f8b6d7427c2c810b8999aee33369ef
Jaecobd1 <jaecob.d1@gmail.com>
2022-11-03 16:07:08 -0400

    adding all files

---
+++ app/app.js
@@ -0,0 +1,109 @@
+var GAMES = [{
+        gameTitle: "Mario Kart 8 Deluxe",
+        gameThumbImg: "MK_150.png",
+        gameFullImg: "MK_300.png",
+        gameBriefDescription: "Hit the road with the definitive version of Mario Kart 8 and play anytime, anywhere. Race your friends or battle them in a revised battle mode for new and returning battle courses",
+        gameDescription: "Hit the road with the definitive version of Mario Kart 8 and play anytime, any-where! Race your friends or battle them in a revised battle mode on new and returning battle courses. Play locally in up to 4-player multiplayer in 1080p while playing in TV Mode. Every track from the Wii U version, including DLC, makes a glorious return. Plus, the Inklings appear as all-new guest characters, along with returning favorites, such as King Boo, Dry Bones, and Bowser Jr.",
+        gameRating: "E"
+    },
+    {
+        gameTitle: "The Legend of Zelda: Breath of the Wild",
+        gameThumbImg: "LZ_150.png",
+        gameFullImg: "LZ_300.png",
+        gameBriefDescription: "Step into a world of discovery, exploration and adventure in The Legend of Zelda: Breath of the Wild",
+        gameDescription: "Forget everything you know about The Legend of Zelda games. Step into a world of discovery, exploration and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking game in the acclaimed series.",
+        gameRating: "E"
+    },
+    {
+        gameTitle: "Super Smash Bros - Ultimate",
+        gameThumbImg: "SB_150.png",
+        gameFullImg: "SB_300.png",
+        gameBriefDescription: "A new game in the Super Smash Bros. series releases on December 7th, 2018",
+        gameDescription: "New fighters, new stages and every fighter ever featured in the series EVER Elevate the Super Smash Bros.experience to new heights with faster combat, new attacks, new items, and new defensive options ",
+        gameRating: "E10+"
+    },
+    {
+        gameTitle: "Super Mario Odyssey",
+        gameThumbImg: "SM_150.png",
+        gameFullImg: "SM_300.png",
+        gameBriefDescription: "Explore 3D kingdoms filled with secrets and surprises, including costumes for Mario and several ways to interact with environments",
+        gameDescription: "Embark on a captivating, globe-trotting adventure. Join Mario on a massive, globe - trotting 3 D adventure and use his incredible new abilities to collect Moons so you can power up your airship, the Odyssey, and rescue Princess Peach from Bowser 's wedding plans. This sandbox-style 3D Mario adventure and mdash;the first since 1996's beloved Super Mario 64 and 2002 's Nintendo GameCube classic Super Mario Sunshine and mdash;is packed with secrets and surprises, and with Mario\'s new moves like cap throw, cap jump, and capture, you 'll have fun and exciting gameplay experiences unlike anything you've enjoyed in a Mario game before.Get ready to be whisked away to strange and amazing places far from the Mushroom Kingdom.",
+        gameRating: "E10+"
+    },
+    {
+        gameTitle: "Carnival Games ",
+        gameThumbImg: "CG_150.png",
+        gameFullImg: "CG_300.png",
+        gameBriefDescription: "Play, win, get rewards: Play games, win tickets, and trade them in for new outfits for your carnival guest characters. Dress them up to match Your style or be as wild as you can with wacky headgear, shirts and bottoms.",
+        gameDescription: "Carnival Games brings the Party back for the entire family – now for the first time on Nintendo Switch With both fan favorite and brand new mini games all in one package, Carnival Games offers fun for everyone and can be played alone or with up to three others. Hit home runs, pile the highest stack of cakes, and take Your horse to the finish line. 20 total mini games will have you and Your friends stepping right up to win again and again. And with Nintendo Switch, the Carnival Games experience can be enjoyed wherever, whenever. A FUN VARIETY OF CARNIVAL GAMES FOR EVERYONE: Like a real life county fair, Carnival Games bursts with exciting mini games that you can play over and over.Classic mini games such as shooting basketballs(“Swish”) and taking down unfriendly clowns(“Clowning Around”) are joined by brand new attractions like flying drone racing(“Light Speed”) and cosmic bowling(“Cosmic Strike”).With 20 total games, Carnival Games is a fantastic time for all. CARNIVAL GAMES EVERYWHERE: With Nintendo Switch, Carnival Games is a game for everyone and for everywhere.Whether on the big screen or small screen, Carnival Games can be enjoyed wherever you want to play.",
+        gameRating: "E10+"
+    },
+    {
+        gameTitle: "Mario Party Superstars",
+        gameThumbImg: "MP_150.png",
+        gameFullImg: "MP_300.png",
+        gameBriefDescription: "Bring the party on 5 classic boards from the Nintendo 64 Mario Party games",
+        gameDescription: "A superstar collection of classic Nintendo 64 boards Calling all superstars!The Mario Party series is back with 5 classic boards from the Nintendo 64 Mario Party games.Race to get the most stars(and sabotage your opponents) on boards like the stellar Space Land or the spooky Horror Land!Party at your own pace by suspending a game and picking up right where you left off!The tides can turn quickly in Mario Party, so stay vigilant, partygoers.All modes can be played online * , too! Play 100 classic minigames from the Nintendo 64 and Nintendo GameCube games and more in Mt.Minigames! Try your hand at a superstar collection of minigames from throughout the Mario Party series.Whether you are saving up coins for stars in board game mode or enjoy one of the 7 modes included in Mt.Minigames, there is nothing quite like being the last one standing in Bumper Balls or the first team making it out of the darkness in Dungeon Duos.All minigames are played with button controls, so you can stick to the Joy - Con controller or bust out the Nintendo Switch Pro Controller or a Nintendo Switch Lite system. Party with superstars near or far Bring the party anytime, anywhere with same - system ** , local wireless ** , or online * play.Use stickers to praise or pester fellow players as you race across the board collecting stars.All modes support online multiplayer, so you can hop in with friends for a few minigames or a marathon 30 - turn match.Victory is just as sweet(and stealing stars is just as satisfying) from anywhere across the globe.",
+        gameRating: "E10+"
+    },
+    {
+        gameTitle: "Minecraft",
+        gameThumbImg: "MC_150.png",
+        gameFullImg: "MC_300.png",
+        gameBriefDescription: "Minecraft is a game about placing blocks and going on adventures",
+        gameDescription: "Minecraft is a game about placing blocks and going on adventures. Explore randomly generated worlds and build amazing things from the simplest of homes to the grandest of castles.Play in creative mode with unlimited resources or mine deep into the world in survival mode, crafting weapons and armor to fend off the dangerous mobs.",
+        gameRating: "E10+"
+    },
+    {
+        gameTitle: "Nintendo Switch Sports",
+        gameThumbImg: "SS_150.png",
+        gameFullImg: "SS_300.png",
+        gameBriefDescription: "Swing, kick, spike, and bowl your way to victory in 6 sports",
+        gameDescription: "Swing, kick, spike, and bowl your way to victory in a sports collection that will get the whole family moving Get moving with Soccer, Volleyball, Bowling, Tennis, Badminton, and Chambara(swordplay) using Joy - Con controllers!Controls are intuitive, so you can hit the court(or lanes, or field, or arena) andget started.With controlled motions you can curve your bowling ball, add spin to a tennis shot, or even use a Joy - Con with the included Leg Strap accessory to kick the ball in a Soccer Shoot - Out.Family and friends can join in on the fun on the same system * or online ** .Plus a seventh sport is planned to be added via a free update this fall;get ready to Golf!",
+        gameRating: "E10+"
+    },
+    {
+        gameTitle: "Animal Crossing: New Horizons",
+        gameThumbImg: "AC_150.png",
+        gameFullImg: "AC_300.png",
+        gameBriefDescription: "Build your community from scratch on a deserted island brimming with possibility; Create your personal getaway and customize your character, home, decorations, and even the landscape itself",
+        gameDescription: "Escape to a deserted island and create your own paradise as you explore, create, and customize in the Animal Crossing: New Horizons game. Your island getaway has a wealth of natural resources that can be used to craft everything from tools to creature comforts. You can hunt down insects at the crack of dawn, decorate your paradise throughout the day, or enjoy sunset on the beach while fishing in the ocean. The time of day and season match real life, so each day on your island is a chance to check in and find new surprises all year round. Show off your island utopia to family and friends— or pack your bags and visit theirs.Whether playing online or with others beside you, island living is even better when you can share it.Even without hopping on a flight, you’ ll meet a cast of charming animal residents bursting with personality.Friendly faces like Tom Nook and Isabelle will lend their services and happily help you grow your budding community.Escape to your island getaway— however, whenever, and wherever you want.",
+        gameRating: "E"
+    },
+    {
+        gameTitle: "Just Dance 2022",
+        gameThumbImg: "JD_150.png",
+        gameFullImg: "JD_300.png",
+        gameBriefDescription: "Just Dance 2022, the ultimate dance game, is back with new environments and 40 hot new tracks from chart-topping hits like \"Don\'t Go Yet\" by Camila Cabello, \"Love Story(Taylor\ 's Version)\" by Taylor Swift, \"Happier Than Ever\" by Billie Eilish, and more!",
+        gameDescription: "Exercise while having fun and start your own your routine with Just Dance Unlimited Sweat mode.\nTeam up with your dance - on - demand streaming service\nEngage the little ones with eight kid - friendly songs and choreographies with Kids mode.\nJust Dance Standard Edition includes a 1 - month free trial of Just Dance Unlimited ",
+        gameRating: "E10+"
+
+    }
+];
+
+
+$(document).ready(function() {
+    loadData();
+})
+
+function loadData() {
+    $.each(GAMES, function(index, game) {
+        $("#app").append(` 
+        <div class="game-holder" id="${index}">
+            <h4>${game.gameTitle}</h4>
+            <div class="game-image">
+                <img src="images/${game.gameThumbImg}" alt="">
+            </div>
+            <div class="brief-rating">
+                <div class="brief-des">
+                    ${game.gameBriefDescription}
+                </div>
+                <div class="rating">Rating: ${game.gameRating}</div>
+                </div>
+        </div>`);
+    })
+
+    // for (let i = 0; i < GAMES.length; i++) {
+    //     console.log(GAMES[i].gameTitle);
+    // }
+}
